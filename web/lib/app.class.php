259a05506ebde11003b8f22587cc51c917ac3a59
Gleb <gleb.godovanyuk@gmail.com>
2016-05-05 06:56:19 +0300

    2

--- web/lib/app.class.php
+++ web/lib/app.class.php
@@ -14,8 +14,6 @@ class App{
 
         self::$router = new Router($uri);
 		
-		throw new Exception('Cannot connect to database');
-		
         self::$db = new DB(Config::get('db.host'), Config::get('db.user'), Config::get('db.password'), Config::get('db.db_name'));
 
         Lang::load(self::$router->getLanguage());
--- web/webroot/index.php
+++ web/webroot/index.php
@@ -5,8 +5,9 @@ require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'common.php');
 require_once(ROOT.DS.'lib'.DS.'init.php');
 
 //session_start();
-App::run($_SERVER['REQUEST_URI']);
+//App::run($_SERVER['REQUEST_URI']);
 
+echo "SUKA NE RABOTAET";
 
 
 
