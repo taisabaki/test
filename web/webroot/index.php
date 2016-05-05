e88fe7538afe35011115a474f089c0c47a6b2496
Gleb <gleb.godovanyuk@gmail.com>
2016-05-05 06:01:44 +0300

    2

--- web/webroot/index.php
+++ web/webroot/index.php
@@ -1,27 +1,12 @@
 <?php
 
-//define('DS', DIRECTORY_SEPARATOR);
-//define('SL', '/');
-//define('ROOT', dirname(dirname(__FILE__)));
-//define('VIEWS_PATH', ROOT.DS.'views');
-//define('CONTROLLERS_PATH', ROOT.DS.'controllers');
-//
-//define('SCRIPT_PATH', DS.'js');
-//define('STYLE_PATH', DS.'css');
+require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'common.php');
 
-//require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'common.php');
-
+echo VIEWS_PATH;
 //require_once(ROOT.DS.'lib'.DS.'init.php');
 
 //session_start();
 //App::run($_SERVER['REQUEST_URI']);
-
-$a = array(1, 2, 3, 4, 5);
-
-foreach($a){
-	echo $b;
-}
-
 
 
 
