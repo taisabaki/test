aca59a2b7948e1aeda1ac3a3c777b59c1d5c664b
Gleb <gleb.godovanyuk@gmail.com>
2016-05-05 06:09:49 +0300

    changed

--- web/webroot/index.php
+++ web/webroot/index.php
@@ -4,10 +4,10 @@ require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'common.php');
 
 require_once(ROOT.DS.'lib'.DS.'init.php');
 
-echo Config::get('site_name');
-
 //session_start();
 //App::run($_SERVER['REQUEST_URI']);
+
+echo $_SERVER['REQUEST_URI'];
 
 
 
